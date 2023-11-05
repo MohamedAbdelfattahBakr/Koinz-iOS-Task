0569b2ab8d1788f924a21c6c7afca325dbf049f3
Mohamed Bakr <mohamed.m.bakr@warababank.com>
2023-11-05 19:10:25 +0200

    Initial Commit

---
+++ Koinz-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Koinz-Task
+//
+//  Created by Mohamed Bakr on 05/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
